e6ed32cc87a092c9b82798c3700909739f6be68b
thomasjpeoples <55585074+thomasjpeoples@users.noreply.github.com>
2022-11-22 14:40:28 +0000

    Update streamlit_app.py
    adding some "vice" and GET fruity ;-)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
 import pandas
+import requests
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 streamlit.title("Regina's Healthy Diner")
 
@@ -13,8 +15,14 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado on toast')   
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+# New section to display the fruityvice
+streamlit.header('Fruityvice = Fruit + Advice')
+streamlit.text(fruityvice_response.json())
