5920c2c1ebbe2da3c8b99cf558766e0b5f4f45c6
thomasjpeoples <55585074+thomasjpeoples@users.noreply.github.com>
2022-11-22 15:36:52 +0000

    Update streamlit_app.py
    Adding in which warehouse to use in execution

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
+my_cur.execute("use warehouse pc_rivery_wh")
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
