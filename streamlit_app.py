a8ed4ca83f277cbac693f03d5f1a606e958e3b87
thomasjpeoples <55585074+thomasjpeoples@users.noreply.github.com>
2022-11-22 11:10:51 +0000

    Update streamlit_app.py
    updated title and header to personally reflect developer

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 
-streamlit.title('My Mom''s New Healthy Diner')
+streamlit.title('Regina's Healthy Diner')
 
-streamlit.header('Breakfast Favourites')
+streamlit.header('Hipster Brunch Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
